27abb065f2f828cc9d331d84d430eb8d34b896e8
hashika-m <hashika0302@gmail.com>
2026-03-18 21:02:08 +0530

    cookie parser

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const PORT=process.env.PORT ||8000
 // // middleware
 // app.use(cors())
 app.use(express.json())
+app.use(cookieParser()) 
 app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials:true
